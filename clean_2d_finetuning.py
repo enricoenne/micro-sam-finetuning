94ed56dfaf5d41bb76fc211374ae50bf7088b53c
enricoenne <enrico.negri97@gmail.com>
2025-03-31 14:22:38 +0200

    everthing in big function

--- clean_2d_finetuning.py
+++ clean_2d_finetuning.py
@@ -22,178 +22,186 @@ import micro_sam.training as sam_training
 from micro_sam.sample_data import fetch_tracking_example_data, fetch_tracking_segmentation_data
 from micro_sam.automatic_segmentation import get_predictor_and_segmenter, automatic_instance_segmentation
 
-root_dir = ""
 
-segmentation_gt = 'CELL_comb'
+def finetuning_and_inference(raw_dataset, segmentation_gt, finetuning = True, inference = True):
 
-image_dir = '/group/jug/Enrico/TISSUE_roi_projection'
-image_paths = sorted(glob(os.path.join(image_dir, "*/*_GFP_max.tif")))
+    root_dir = ''
 
-segmentation_dir = '/group/jug/Enrico/TISSUE_roi_projection'
-segmentation_paths = sorted(glob(os.path.join(image_dir, "*/*_" + segmentation_gt + ".tif")))
+    image_dir = '/group/jug/Enrico/TISSUE_roi_projection'
+    image_paths = sorted(glob(os.path.join(image_dir, '*/*_' + raw_dataset + '.tif')))
 
-
-# Load images from multiple files in folder via pattern (here: all tif files)
-raw_key, label_key = "*_GFP_max.tif", "*_" + segmentation_gt + ".tif"
-
-# Alternative: if you have tif stacks you can just set 'raw_key' and 'label_key' to None
-# raw_key, label_key= None, None
-
-'''# The 'roi' argument can be used to subselect parts of the data.
-# Here, we use it to select the first 70 images (frames) for the train split and the other frames for the val split.
-train_roi = np.s_[:70, :, :]
-val_roi = np.s_[70:, :, :]'''
-# already splitted into two folders
-training_dir = os.path.join(image_dir, "training")
-validation_dir = os.path.join(image_dir, "validation")
-
-# Here, we use `micro_sam.training.default_sam_loader` for creating a suitable data loader from
-# the example hela data. You can either adapt this for your own data or write a suitable torch dataloader yourself.
-# Here's a quickstart notebook to create your own dataloaders: https://github.com/constantinpape/torch-em/blob/main/notebooks/tutorial_create_dataloaders.ipynb
-
-batch_size = 1  # the training batch size
-patch_shape = (1, 512, 512)  # the size of patches for training
-
-# Train an additional convolutional decoder for end-to-end automatic instance segmentation
-# NOTE 1: It's important to have densely annotated-labels while training the additional convolutional decoder.
-# NOTE 2: In case you do not have labeled images, we recommend using `micro-sam` annotator tools to annotate as many objects as possible per image for best performance.
-train_instance_segmentation = True
-
-# NOTE: The dataloader internally takes care of adding label transforms: i.e. used to convert the ground-truth
-# labels to the desired instances for finetuning Segment Anythhing, or, to learn the foreground and distances
-# to the object centers and object boundaries for automatic segmentation.
-
-# There are cases where our inputs are large and the labeled objects are not evenly distributed across the image.
-# For this we use samplers, which ensure that valid inputs are chosen subjected to the paired labels.
-# The sampler chosen below makes sure that the chosen inputs have atleast one foreground instance, and filters out small objects.
-sampler = MinInstanceSampler(min_size=25)  # NOTE: The choice of 'min_size' value is paired with the same value in 'min_size' filter in 'label_transform'.
-
-train_loader = sam_training.default_sam_loader(
-    raw_paths=training_dir,
-    raw_key=raw_key,
-    label_paths=training_dir,
-    label_key=label_key,
-    with_segmentation_decoder=train_instance_segmentation,
-    patch_shape=patch_shape,
-    batch_size=batch_size,
-    is_seg_dataset=True,
-    shuffle=True,
-    raw_transform=sam_training.identity,
-    sampler=sampler,
-)
-
-val_loader = sam_training.default_sam_loader(
-    raw_paths=validation_dir,
-    raw_key=raw_key,
-    label_paths=validation_dir,
-    label_key=label_key,
-    with_segmentation_decoder=train_instance_segmentation,
-    patch_shape=patch_shape,
-    batch_size=batch_size,
-    is_seg_dataset=True,
-    shuffle=True,
-    raw_transform=sam_training.identity,
-    sampler=sampler,
-)
-
-# All hyperparameters for training.
-n_objects_per_batch = 5  # the number of objects per batch that will be sampled
-device = "cuda" if torch.cuda.is_available() else "cpu" # the device/GPU used for training
-n_epochs = 5  # how long we train (in epochs)
-
-# The model_type determines which base model is used to initialize the weights that are finetuned.
-# We use vit_b here because it can be trained faster. Note that vit_h usually yields higher quality results.
-model_type = "vit_t_lm"
-
-# The name of the checkpoint. The checkpoints will be stored in './checkpoints/<checkpoint_name>'
-checkpoint_name = segmentation_gt
-
-# run training
-sam_training.train_sam(
-    name=checkpoint_name,
-    save_root=os.path.join(root_dir, "models"),
-    model_type=model_type,
-    train_loader=train_loader,
-    val_loader=val_loader,
-    n_epochs=n_epochs,
-    n_objects_per_batch=n_objects_per_batch,
-    with_segmentation_decoder=train_instance_segmentation,
-    device=device,
-)
-
-# Let's spot our best checkpoint and download it to get started with the annotation tool
-best_checkpoint = os.path.join("models", "checkpoints", checkpoint_name, "best.pt")
+    segmentation_dir = '/group/jug/Enrico/TISSUE_roi_projection'
+    segmentation_paths = sorted(glob(os.path.join(image_dir, "*/*_" + segmentation_gt + ".tif")))
 
 
-def run_automatic_instance_segmentation(image, checkpoint_path, model_type="vit_b_lm", device=None):
-    """Automatic Instance Segmentation (AIS) by training an additional instance decoder in SAM.
+    # Load images from multiple files in folder via pattern (here: all tif files)
+    raw_key, label_key = '*_' + raw_dataset + '.tif', '*_' + segmentation_gt + '.tif'
 
-    NOTE: AIS is supported only for `µsam` models.
+    # Alternative: if you have tif stacks you can just set 'raw_key' and 'label_key' to None
+    # raw_key, label_key= None, None
 
-    Args:
-        image: The input image.
-        checkpoint_path: The path to stored checkpoints.
-        model_type: The choice of the `µsam` model.
-        device: The device to run the model inference.
+    '''# The 'roi' argument can be used to subselect parts of the data.
+    # Here, we use it to select the first 70 images (frames) for the train split and the other frames for the val split.
+    train_roi = np.s_[:70, :, :]
+    val_roi = np.s_[70:, :, :]'''
+    # already splitted into two folders
+    training_dir = os.path.join(image_dir, "training")
+    validation_dir = os.path.join(image_dir, "validation")
 
-    Returns:
-        The instance segmentation.
-    """
-    # Step 1: Get the 'predictor' and 'segmenter' to perform automatic instance segmentation.
-    predictor, segmenter = get_predictor_and_segmenter(
-        model_type=model_type, # choice of the Segment Anything model
-        checkpoint=checkpoint_path,  # overwrite to pass your own finetuned model.
-        device=device,  # the device to run the model inference.
+    # Here, we use `micro_sam.training.default_sam_loader` for creating a suitable data loader from
+    # the example hela data. You can either adapt this for your own data or write a suitable torch dataloader yourself.
+    # Here's a quickstart notebook to create your own dataloaders: https://github.com/constantinpape/torch-em/blob/main/notebooks/tutorial_create_dataloaders.ipynb
+
+    batch_size = 1  # the training batch size
+    patch_shape = (1, 1024, 1024)  # the size of patches for training
+
+    # Train an additional convolutional decoder for end-to-end automatic instance segmentation
+    # NOTE 1: It's important to have densely annotated-labels while training the additional convolutional decoder.
+    # NOTE 2: In case you do not have labeled images, we recommend using `micro-sam` annotator tools to annotate as many objects as possible per image for best performance.
+    train_instance_segmentation = True
+
+    # NOTE: The dataloader internally takes care of adding label transforms: i.e. used to convert the ground-truth
+    # labels to the desired instances for finetuning Segment Anythhing, or, to learn the foreground and distances
+    # to the object centers and object boundaries for automatic segmentation.
+
+    # There are cases where our inputs are large and the labeled objects are not evenly distributed across the image.
+    # For this we use samplers, which ensure that valid inputs are chosen subjected to the paired labels.
+    # The sampler chosen below makes sure that the chosen inputs have atleast one foreground instance, and filters out small objects.
+    sampler = MinInstanceSampler(min_size=25)  # NOTE: The choice of 'min_size' value is paired with the same value in 'min_size' filter in 'label_transform'.
+
+    train_loader = sam_training.default_sam_loader(
+        raw_paths=training_dir,
+        raw_key=raw_key,
+        label_paths=training_dir,
+        label_key=label_key,
+        with_segmentation_decoder=train_instance_segmentation,
+        patch_shape=patch_shape,
+        batch_size=batch_size,
+        is_seg_dataset=True,
+        shuffle=True,
+        raw_transform=sam_training.identity,
+        sampler=sampler,
     )
 
-    # Step 2: Get the instance segmentation for the given image.
-    prediction = automatic_instance_segmentation(
-        predictor=predictor,  # the predictor for the Segment Anything model.
-        segmenter=segmenter,  # the segmenter class responsible for generating predictions.
-        input_path=image,
-        ndim=2,
+    val_loader = sam_training.default_sam_loader(
+        raw_paths=validation_dir,
+        raw_key=raw_key,
+        label_paths=validation_dir,
+        label_key=label_key,
+        with_segmentation_decoder=train_instance_segmentation,
+        patch_shape=patch_shape,
+        batch_size=batch_size,
+        is_seg_dataset=True,
+        shuffle=True,
+        raw_transform=sam_training.identity,
+        sampler=sampler,
     )
 
-    return prediction
+    # All hyperparameters for training.
+    n_objects_per_batch = 5  # the number of objects per batch that will be sampled
+    device = "cuda" if torch.cuda.is_available() else "cpu" # the device/GPU used for training
+    n_epochs = 5  # how long we train (in epochs)
+
+    # The model_type determines which base model is used to initialize the weights that are finetuned.
+    # We use vit_b here because it can be trained faster. Note that vit_h usually yields higher quality results.
+    model_type = "vit_t_lm"
+
+    # The name of the checkpoint. The checkpoints will be stored in './checkpoints/<checkpoint_name>'
+    checkpoint_name = segmentation_gt
+
+    # run training
+    if finetuning:
+        sam_training.train_sam(
+            name=checkpoint_name,
+            save_root=os.path.join(root_dir, "models"),
+            model_type=model_type,
+            train_loader=train_loader,
+            val_loader=val_loader,
+            n_epochs=n_epochs,
+            n_objects_per_batch=n_objects_per_batch,
+            with_segmentation_decoder=train_instance_segmentation,
+            device=device,
+        )
+
+    # Let's spot our best checkpoint and download it to get started with the annotation tool
+    best_checkpoint = os.path.join("models", "checkpoints", checkpoint_name, "best.pt")
 
 
-assert os.path.exists(best_checkpoint), "Please train the model first to run inference on the finetuned model."
-assert train_instance_segmentation is True, "Oops. You didn't opt for finetuning using the decoder-based automatic instance segmentation."
+    def run_automatic_instance_segmentation(image, checkpoint_path, model_type="vit_b_lm", device=None):
+        """Automatic Instance Segmentation (AIS) by training an additional instance decoder in SAM.
 
-# Let's check the first 5 images. Feel free to comment out the line below to run inference on all images.
-image_paths = image_paths[:10]
+        NOTE: AIS is supported only for `µsam` models.
 
-oputput_dir = os.path.join('finetuning_test', checkpoint_name)
-if not os.path.exists(oputput_dir):
-    os.makedirs(oputput_dir)
+        Args:
+            image: The input image.
+            checkpoint_path: The path to stored checkpoints.
+            model_type: The choice of the `µsam` model.
+            device: The device to run the model inference.
 
-for image_path, segmentation_path in zip(image_paths, segmentation_paths):
-    image_name = image_path.split('/')[-1][:-4]
-    seg_name = segmentation_path.split('/')[-1][:-4]
-    print(image_name)
-    image = imageio.imread(image_path)
-    segmentation = imageio.imread(segmentation_path)
+        Returns:
+            The instance segmentation.
+        """
+        # Step 1: Get the 'predictor' and 'segmenter' to perform automatic instance segmentation.
+        predictor, segmenter = get_predictor_and_segmenter(
+            model_type=model_type, # choice of the Segment Anything model
+            checkpoint=checkpoint_path,  # overwrite to pass your own finetuned model.
+            device=device,  # the device to run the model inference.
+        )
 
-    # Predicted instances
-    prediction = run_automatic_instance_segmentation(
-        image=image, checkpoint_path=best_checkpoint, model_type=model_type, device=device
-    )
+        # Step 2: Get the instance segmentation for the given image.
+        prediction = automatic_instance_segmentation(
+            predictor=predictor,  # the predictor for the Segment Anything model.
+            segmenter=segmenter,  # the segmenter class responsible for generating predictions.
+            input_path=image,
+            ndim=2,
+        )
 
-    # Visualize the predictions
-    fig, ax = plt.subplots(1, 3, figsize=(10, 6))
+        return prediction
 
-    ax[0].imshow(image, cmap="gray", vmin=np.amin(image), vmax=np.amax(image)/4)
-    ax[0].axis("off")
-    ax[0].set_title(image_name)
+    if inference:
+        assert os.path.exists(best_checkpoint), "Please train the model first to run inference on the finetuned model."
+        assert train_instance_segmentation is True, "Oops. You didn't opt for finetuning using the decoder-based automatic instance segmentation."
 
-    ax[1].imshow(prediction, cmap=get_random_colors(prediction), interpolation="nearest")
-    ax[1].axis("off")
-    ax[1].set_title("Predictions (AIS)")
+        # Let's check the first 5 images. Feel free to comment out the line below to run inference on all images.
+        image_paths = image_paths[:10]
 
-    ax[2].imshow(segmentation, cmap=get_random_colors(prediction), interpolation="nearest")
-    ax[2].axis("off")
-    ax[2].set_title(seg_name)
+        oputput_dir = os.path.join('finetuning_test', checkpoint_name)
+        if not os.path.exists(oputput_dir):
+            os.makedirs(oputput_dir)
 
-    plt.savefig(os.path.join(oputput_dir, image_name + '.png'), bbox_inches='tight')
-    #plt.show()
-    plt.close()
+        for image_path, segmentation_path in zip(image_paths, segmentation_paths):
+            image_name = image_path.split('/')[-1][:-4]
+            seg_name = segmentation_path.split('/')[-1][:-4]
+            print(image_name)
+            image = imageio.imread(image_path)
+            segmentation = imageio.imread(segmentation_path)
+
+            # Predicted instances
+            prediction = run_automatic_instance_segmentation(
+                image=image, checkpoint_path=best_checkpoint, model_type=model_type, device=device
+            )
+
+            # Visualize the predictions
+            fig, ax = plt.subplots(1, 3, figsize=(10, 6))
+
+            ax[0].imshow(image, cmap="gray", vmin=np.amin(image), vmax=np.amax(image)/4)
+            ax[0].axis("off")
+            ax[0].set_title(image_name)
+
+            ax[1].imshow(prediction, cmap=get_random_colors(prediction), interpolation="nearest")
+            ax[1].axis("off")
+            ax[1].set_title("Predictions (AIS)")
+
+            ax[2].imshow(segmentation, cmap=get_random_colors(prediction), interpolation="nearest")
+            ax[2].axis("off")
+            ax[2].set_title(seg_name)
+
+            plt.savefig(os.path.join(oputput_dir, image_name + '.png'), bbox_inches='tight')
+            #plt.show()
+            plt.close()
+
+
+raw = 'GFP_max'
+seg = 'CELL_comb'
+
+finetuning_and_inference(raw, seg)
